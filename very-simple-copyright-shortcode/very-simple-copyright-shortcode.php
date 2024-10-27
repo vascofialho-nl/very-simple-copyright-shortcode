d855d5277927400e0f7e09c87f1c6d2bb34e5a89
Vasco J. Fialho Mascarenhas de Carvalho <info@vascofialho.nl>
2024-10-27 16:39:14 +0100

    Update very-simple-copyright-shortcode.php
    update version number

--- very-simple-copyright-shortcode/very-simple-copyright-shortcode.php
+++ very-simple-copyright-shortcode/very-simple-copyright-shortcode.php
@@ -8,7 +8,7 @@ Plugin Name: Very Simple Copyright Shortcode
 Plugin URI: http://www.vascofialho.nl
 Description: Display copyright information with a shortcode.
 Author: vascofmdc
-Version: 1.0
+Version: 1.1
 Author URI: http://www.vascofialho.nl
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
