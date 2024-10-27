c720d5749511afee5efcb36dfdab4e252f340278
Vasco J. Fialho Mascarenhas de Carvalho <info@vascofialho.nl>
2024-10-27 17:11:44 +0100

    more testing

--- very-simple-copyright-shortcode/updater.php
+++ very-simple-copyright-shortcode/updater.php
@@ -4,33 +4,56 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-// Define constants for version control
-define( 'VSCS_PLUGIN_VERSION', '1.0' );
-define( 'VSCS_PLUGIN_NAME', 'very-simple-copyright-shortcode' );
-define( 'VSCS_GITHUB_URL', 'https://github.com/vascofialho-nl/very-simple-copyright-shortcode' );
+class VSCS_Plugin_Updater {
+    private $api_url = 'https://api.github.com/repos/vascofialho-nl/very-simple-copyright-shortcode/releases/latest';
+    private $plugin_file;
+    private $plugin_slug;
+    private $version;
 
+    public function __construct( $plugin_file, $version ) {
+        $this->plugin_file = $plugin_file;
+        $this->plugin_slug = plugin_basename( $plugin_file );
+        $this->version = $version;
 
-// Check for updates from GitHub
-function vscs_check_for_updates() {
-    $response = wp_remote_get( VSCS_GITHUB_URL . '/releases/latest' );
-    
-    if ( is_wp_error( $response ) ) {
-        return; // Exit if there's an error reaching GitHub
+        add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'check_for_update' ] );
+        add_filter( 'plugins_api', [ $this, 'plugin_info' ], 20, 3 );
     }
 
-    $data = json_decode( wp_remote_retrieve_body( $response ), true );
+    public function check_for_update( $transient ) {
+        if ( empty( $transient->checked ) ) return $transient;
 
-    if ( isset( $data['tag_name'] ) && version_compare( $data['tag_name'], VSCS_PLUGIN_VERSION, '>' ) ) {
-        add_action( 'admin_notices', 'vscs_display_update_notice' );
+        $current_version = $this->version;
+        $remote_version = $this->get_latest_version();
+
+        if ( version_compare( $current_version, $remote_version, '<' ) ) {
+            $plugin_data = get_plugin_data( $this->plugin_file );
+            $transient->response[ $this->plugin_slug ] = (object) [
+                'slug'        => $this->plugin_slug,
+                'new_version' => $remote_version,
+                'package'     => $this->get_latest_download_url(),
+                'url'         => $plugin_data['PluginURI'],
+            ];
+        }
+        return $transient;
+    }
+
+    private function get_latest_version() {
+        $response = wp_remote_get( $this->api_url );
+        if ( is_wp_error( $response ) ) return false;
+
+        $data = json_decode( wp_remote_retrieve_body( $response ) );
+        return ! empty( $data->tag_name ) ? $data->tag_name : false;
+    }
+
+    private function get_latest_download_url() {
+        $response = wp_remote_get( $this->api_url );
+        if ( is_wp_error( $response ) ) return false;
+
+        $data = json_decode( wp_remote_retrieve_body( $response ) );
+        return ! empty( $data->assets[0]->browser_download_url ) ? $data->assets[0]->browser_download_url : false;
     }
 }
-add_action( 'admin_init', 'vscs_check_for_updates' );
 
-// Display an update notice in the dashboard
-function vscs_display_update_notice() {
-    echo '<div class="notice notice-warning is-dismissible">';
-    echo '<p>A new version of Very Simple Copyright Shortcode is available. <a href="' . VSCS_GITHUB_URL . '/releases/latest" target="_blank">Update now</a>.</p>';
-    echo '</div>';
-}
+new VSCS_Plugin_Updater( __FILE__, '1.0' ); // Replace '1.0' with the current plugin version.
 
 /* EOF =============================================================================================================================================================== */
--- very-simple-copyright-shortcode/very-simple-copyright-shortcode.php
+++ very-simple-copyright-shortcode/very-simple-copyright-shortcode.php
@@ -8,7 +8,7 @@ Plugin Name: Very Simple Copyright Shortcode
 Plugin URI: http://www.vascofialho.nl
 Description: Display copyright information with a shortcode.
 Author: vascofmdc
-Version: 1.0
+Version: 0.1
 Author URI: http://www.vascofialho.nl
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -21,6 +21,9 @@ require_once(plugin_dir_path(__FILE__) . 'install.php');
 
 // Include updater system (connected with github)
 require_once plugin_dir_path( __FILE__ ) . 'updater.php';
+define( 'VSCS_PLUGIN_VERSION', '1.0' ); // Adjust this when you release a new version.
+new VSCS_Plugin_Updater( __FILE__, VSCS_PLUGIN_VERSION );
+
 
 // Add a menu item to the dashboard
 	function vscs_add_menu_item() {
